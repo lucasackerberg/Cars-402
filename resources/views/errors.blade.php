d20410dbc5b1b37fb8fb12a8c5f6602008392f8d
lucasackerberg <lucas@ackerberg.se>
2024-02-26 11:44:26 +0100

    login functionality
    basic login functionality inplemented

---
+++ resources/views/errors.blade.php
@@ -0,0 +1,5 @@
+@if ($errors->any())
+<p>
+    <u>{{ $errors->first() }}</u>
+</p>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::post('/login', LoginController::class);
