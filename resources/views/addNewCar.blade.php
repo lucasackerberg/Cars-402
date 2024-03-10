d2724591ee991dd0f413479397aaecbe4ee12a38
Karolina <karolina.limanowska@gmail.com>
2024-03-10 22:32:32 +0100

    styling buttons and forms

--- resources/views/addNewCar.blade.php
+++ resources/views/addNewCar.blade.php
@@ -1,36 +1,38 @@
 @include('header')
 @include('navbar')
+
 <div class="addNewCarWrapper">
     <div class="addNewCarForm">
         <form action="/createCar" method="POST" class="createCarForm">
             @csrf
             <h2>Add new car</h2>
-            <div >
+            <div class="form-group">
                 <label for="brand">Brand</label>
-                <input name="brand" type="text">
+                <input name="brand" type="text" class="form-control">
             </div>
-            <div >
+            <div class="form-group">
                 <label for="model">Model</label>
-                <input name="model" type="text">
+                <input name="model" type="text" class="form-control">
             </div>
-            <div >
+            <div class="form-group">
                 <label for="year">Year</label>
-                <input name="year" type="text">
+                <input name="year" type="text" class="form-control">
             </div>
-            <div >
+            <div class="form-group">
                 <label for="color">Color</label>
-                <input name="color" type="text">
+                <input name="color" type="text" class="form-control">
             </div>
-            <div >
+            <div class="form-group">
                 <label for="registration">Registration</label>
-                <input name="registration" type="text">
+                <input name="registration" type="text" class="form-control">
             </div>
-            <div >
+            <div class="form-group">
                 <label for="problem_description">Problem Description</label>
-                <textarea name="problem_description"></textarea>
+                <textarea name="problem_description" class="form-control"></textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Create Car</button>
+            <button type="submit" class="btn btn-danger btn-lg">Create Car</button>
         </form>
     </div>
 </div>
+
 @include('footer')
--- resources/views/viewCar.blade.php
+++ resources/views/viewCar.blade.php
@@ -1,41 +1,45 @@
 @include('header')
 @include('navbar')
+
 <div class="addNewCarWrapper">
     <div class="addNewCarForm">
         <form action="{{ route('updateCar', ['car' => $car->id]) }}" method="POST" class="updateCarForm">
             @csrf
-            <h2>Edit car</h2>
-            <div>
+                <h2>Edit car</h2>
+                <form action="{{ route('destroy', ['car' => $car->id]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-dark btn-sm">Delete</button>
+            </form>
+
+            <div class="form-group">
                 <label for="brand">Brand</label>
-                <input name="brand" type="text" value="{{ $car->brand }}">
+                <input name="brand" type="text" value="{{ $car->brand }}" class="form-control">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="model">Model</label>
-                <input name="model" type="text" value="{{ $car->model }}">
+                <input name="model" type="text" value="{{ $car->model }}" class="form-control">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="year">Year</label>
-                <input name="year" type="text" value="{{ $car->year }}">
+                <input name="year" type="text" value="{{ $car->year }}" class="form-control">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="color">Color</label>
-                <input name="color" type="text" value="{{ $car->color }}">
+                <input name="color" type="text" value="{{ $car->color }}" class="form-control">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="registration">Registration</label>
-                <input name="registration" type="text" value="{{ $car->registration }}">
+                <input name="registration" type="text" value="{{ $car->registration }}" class="form-control">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="problem_description">Problem Description</label>
-                <textarea name="problem_description">{{ $car->problem_description }}</textarea>
+                <textarea name="problem_description" class="form-control">{{ $car->problem_description }}</textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Update Car</button>
+            <button type="submit" class="btn btn-danger btn-lg">Update</button>
         </form>
-        <form action="{{ route('destroy', ['car' => $car->id]) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-primary">Delete Car</button>
-        </form>
+       
     </div>
 </div>
+
 @include('footer')
