e03b53376d3ed8952426031e65ede8122feedd6f
Karolina <karolina.limanowska@gmail.com>
2024-03-12 22:05:37 +0100

    login test

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
@@ -17,7 +18,7 @@ class LoginController extends Controller
         if(Auth::attempt($credentials)){
             return redirect('/dashboard');
         } else {
-            return redirect()->back()->with('error', 'Whoops! Please try to login again.');
+            return redirect()->back()->with('error', 'Whoops! Please try to login again.')->setStatusCode(401);
         }
     }
 }
