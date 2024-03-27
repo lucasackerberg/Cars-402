2b9fa9480a9f29063ed899de81f14627c0c87542
Karolina <karolina.limanowska@gmail.com>
2024-03-27 10:48:54 +0100

    adding more exhaustive error messages and required to forms. also restructured html to move title and success out of the form in viewCar

--- resources/views/addNewCar.blade.php
+++ resources/views/addNewCar.blade.php
@@ -8,30 +8,45 @@
             <h2>Add new car</h2>
             <div class="form-group">
                 <label for="brand">Brand</label>
-                <input name="brand" type="text" class="form-control">
+                <input name="brand" type="text" required class="form-control">
+                @error('brand')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="model">Model</label>
-                <input name="model" type="text" class="form-control">
+                <input name="model" type="text" required class="form-control">
+                @error('model')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="year">Year</label>
-                <input name="year" type="text" class="form-control">
+                <input name="year" type="text" required class="form-control">
                 @error('year')
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="color">Color</label>
-                <input name="color" type="text" class="form-control">
+                <input name="color" type="text" required class="form-control">
+                @error('color')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="registration">Registration</label>
-                <input name="registration" type="text" class="form-control">
+                <input name="registration" type="text" required class="form-control">
+                @error('registration')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="problem_description">Problem Description</label>
-                <textarea name="problem_description" class="form-control"></textarea>
+                <textarea name="problem_description" required class="form-control"></textarea>
+                @error('problem_description')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </div>
             <button type="submit" class="btn btn-danger btn-lg">Add</button>
         </form>
--- resources/views/viewCar.blade.php
+++ resources/views/viewCar.blade.php
@@ -4,8 +4,7 @@
 
 <div class="addNewCarWrapper">
     <div class="addNewCarForm">
-        <form action="{{ route('updateCar', ['car' => $car->id]) }}" method="POST" class="updateCarForm">
-            @csrf
+
 
             <h2 class="editCar">Edit car</h2>
 
@@ -14,34 +13,50 @@
                     {{ session()->get('success') }}
                 </div>
             @endif
-                
+            <form action="{{ route('updateCar', ['car' => $car->id]) }}" method="POST" class="updateCarForm">
+            @csrf
             <div class="form-group">
                 <label for="brand">Brand</label>
                 <input name="brand" type="text" value="{{ $car->brand }}" class="form-control">
             </div>
+            @error('brand')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
             <div class="form-group">
                 <label for="model">Model</label>
-                <input name="model" type="text" value="{{ $car->model }}" class="form-control">
+                <input name="model" type="text" required value="{{ $car->model }}" class="form-control">
             </div>
+            @error('model')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
             <div class="form-group">
                 <label for="year">Year</label>
-                <input name="year" type="text" value="{{ $car->year }}" class="form-control">
+                <input name="year" type="text" required value="{{ $car->year }}" class="form-control">
             </div>
             @error('year')
                 <span class="text-danger">{{ $message }}</span>
             @enderror
             <div class="form-group">
                 <label for="color">Color</label>
-                <input name="color" type="text" value="{{ $car->color }}" class="form-control">
+                <input name="color" type="text" required value="{{ $car->color }}" class="form-control">
             </div>
+            @error('color')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
             <div class="form-group">
                 <label for="registration">Registration</label>
-                <input name="registration" type="text" value="{{ $car->registration }}" class="form-control">
+                <input name="registration" type="text" required value="{{ $car->registration }}" class="form-control">
             </div>
+            @error('registration')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
             <div class="form-group">
                 <label for="problem_description">Problem Description</label>
-                <textarea name="problem_description" class="form-control">{{ $car->problem_description }}</textarea>
+                <textarea name="problem_description" required class="form-control">{{ $car->problem_description }}</textarea>
             </div>
+            @error('problem_description')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
             <button type="submit" class="btn btn-danger btn-lg">Update</button>
         </form>
 
